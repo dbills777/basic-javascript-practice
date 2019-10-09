e725fbc386c7e8a79341e849f12feb2dc42ec6bf
dbills777 <dbills777@gmail.com>
2019-10-08 18:34:58 -0600

    CREATED LIST OF MOVIE NAMES

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 import { films } from '/assets/films.js';
-console.log(films);
-console.log(films[0].title);
-console.log(films[0].opening_crawl);
+let starWarsTitles = document.querySelector('.movie-titles');
+// console.log(films);
+// console.log(films[0].title);
+// console.log(films[0].opening_crawl);
 let mainSection = document.querySelector('main');
+// console.log(films[0].characters)
+
+// console.log(Array.isArray(films));
+// console.log(films[1].title);
+
+
+//create new array with film titles in them 
+const movieNames = films.map((film) => {
+    return film.title
+})
+//this logs all the movie titles in the new array called movie names/
+console.log(movieNames);
+for (let i = 0; i < movieNames.length; i++) {
+    console.log(movieNames[i])
+    // document.createElement('div').textContent = movieNames[i];
+    starWarsTitles.textContent = movieNames;
+    // mainSection.appendChild(movieNames[i])
+
+
+
+
+}
+// movieNames.forEach(mainSection.appendChild(movieNames))
+// SET DOM ELEMENTS FORM THE ASSET LIST
+// starWarsTitles.textContent = movieNames[0];
+
 let film1 = document.createElement('div');
 let film2 = document.createElement('div');
 let film3 = document.createElement('div');
