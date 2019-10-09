5684e508d2446d48f598488089a57f191c59a29a
dbills777 <dbills777@gmail.com>
2019-10-08 19:26:24 -0600

    end of the day 10-8-2019

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
 import { films } from '/assets/films.js';
 let starWarsTitles = document.querySelector('.movie-titles');
-// console.log(films);
-// console.log(films[0].title);
-// console.log(films[0].opening_crawl);
-let mainSection = document.querySelector('main');
-// console.log(films[0].characters)
-
-// console.log(Array.isArray(films));
-// console.log(films[1].title);
-
 
 //create new array with film titles in them 
 const movieNames = films.map((film) => {
     return film.title
 })
+const crawls = films.map((film) => {
+    return film.crawl
+})
+console.log(crawls);
+
+
 //this logs all the movie titles in the new array called movie names/
+//ADDS ALL TITLES IN A SINGLE H1
 console.log(movieNames);
 for (let i = 0; i < movieNames.length; i++) {
     console.log(movieNames[i])
-    // document.createElement('div').textContent = movieNames[i];
     starWarsTitles.textContent = movieNames;
-    // mainSection.appendChild(movieNames[i])
 }
 
 
-// movieNames.forEach(mainSection.appendChild(movieNames))
-// SET DOM ELEMENTS FORM THE ASSET LIST
-// starWarsTitles.textContent = movieNames[0];
 
-
+//set variables to make it easier to read the code
 let childElement;
 let appendChildElement;
 let parentElement;
 
-// Bind to id bookmarks
+
+// Bind to id movie-titles Div
+//MEDIUM ARTICLE OUTLINING PROCESS IN A SIMILAR WAY.
+// https://codeburst.io/learn-how-to-create-html-elements-with-plain-javascript-4f1323f96252
+//ADDS ALL TITLES IN INDIVIDUAL DIVS----------------------------------
 parentElement = document.getElementById('movie-titles');
 console.log(films[0])
-// Add child elements
 for (let i = 0; i < movieNames.length; i++) {
     console.log(movieNames[i])
     childElement = document.createElement('div');
@@ -44,31 +40,4 @@ for (let i = 0; i < movieNames.length; i++) {
     appendChildElement.innerHTML = films[i].title;
 
 }
-
-
-
-// let film1 = document.createElement('div');
-// let film2 = document.createElement('div');
-// let film3 = document.createElement('div');
-// let film4 = document.createElement('div');
-// let film5 = document.createElement('div');
-// let film6 = document.createElement('div');
-// let film7 = document.createElement('div');
-
-// film1.textContent = films[0].opening_crawl;
-// film2.textContent = films[1].opening_crawl;
-// film3.textContent = films[2].opening_crawl;
-// film4.textContent = films[3].opening_crawl;
-// film5.textContent = films[4].opening_crawl;
-// film6.textContent = films[5].opening_crawl;
-// film7.textContent = films[6].opening_crawl;
-
-// mainSection.appendChild(film1)
-// mainSection.appendChild(film2)
-// mainSection.appendChild(film3)
-// mainSection.appendChild(film4)
-// mainSection.appendChild(film5)
-// mainSection.appendChild(film6)
-// mainSection.appendChild(film7)
-
 
