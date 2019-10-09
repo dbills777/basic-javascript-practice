668c9b92e08f307b07f04e9ddb36f4e84b31eb36
dbills777 <dbills777@gmail.com>
2019-10-08 19:15:37 -0600

    created divs in HTML for movieNames array

--- main.js
+++ main.js
@@ -21,37 +21,54 @@ for (let i = 0; i < movieNames.length; i++) {
     // document.createElement('div').textContent = movieNames[i];
     starWarsTitles.textContent = movieNames;
     // mainSection.appendChild(movieNames[i])
-
-
-
-
 }
+
+
 // movieNames.forEach(mainSection.appendChild(movieNames))
 // SET DOM ELEMENTS FORM THE ASSET LIST
 // starWarsTitles.textContent = movieNames[0];
 
-let film1 = document.createElement('div');
-let film2 = document.createElement('div');
-let film3 = document.createElement('div');
-let film4 = document.createElement('div');
-let film5 = document.createElement('div');
-let film6 = document.createElement('div');
-let film7 = document.createElement('div');
 
-film1.textContent = films[0].opening_crawl;
-film2.textContent = films[1].opening_crawl;
-film3.textContent = films[2].opening_crawl;
-film4.textContent = films[3].opening_crawl;
-film5.textContent = films[4].opening_crawl;
-film6.textContent = films[5].opening_crawl;
-film7.textContent = films[6].opening_crawl;
+let childElement;
+let appendChildElement;
+let parentElement;
 
-mainSection.appendChild(film1)
-mainSection.appendChild(film2)
-mainSection.appendChild(film3)
-mainSection.appendChild(film4)
-mainSection.appendChild(film5)
-mainSection.appendChild(film6)
-mainSection.appendChild(film7)
+// Bind to id bookmarks
+parentElement = document.getElementById('movie-titles');
+console.log(films[0])
+// Add child elements
+for (let i = 0; i < movieNames.length; i++) {
+    console.log(movieNames[i])
+    childElement = document.createElement('div');
+    appendChildElement = parentElement.appendChild(childElement)
+    appendChildElement.innerHTML = films[i].title;
+
+}
+
+
+
+// let film1 = document.createElement('div');
+// let film2 = document.createElement('div');
+// let film3 = document.createElement('div');
+// let film4 = document.createElement('div');
+// let film5 = document.createElement('div');
+// let film6 = document.createElement('div');
+// let film7 = document.createElement('div');
+
+// film1.textContent = films[0].opening_crawl;
+// film2.textContent = films[1].opening_crawl;
+// film3.textContent = films[2].opening_crawl;
+// film4.textContent = films[3].opening_crawl;
+// film5.textContent = films[4].opening_crawl;
+// film6.textContent = films[5].opening_crawl;
+// film7.textContent = films[6].opening_crawl;
+
+// mainSection.appendChild(film1)
+// mainSection.appendChild(film2)
+// mainSection.appendChild(film3)
+// mainSection.appendChild(film4)
+// mainSection.appendChild(film5)
+// mainSection.appendChild(film6)
+// mainSection.appendChild(film7)
 
 
